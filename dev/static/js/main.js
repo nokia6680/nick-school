d5340530ffdb1760583fab50fe38376ddd3d7f8d
Artur Ballod <artur.ballod@gmail.com>
2021-09-09 01:42:20 +0200

    1

---
+++ dev/static/js/main.js
@@ -0,0 +1,57 @@
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+var firstStep = document.querySelector('.js-start');
+var firstPage = document.querySelector('.header');
+var secondPage = document.querySelector('.content');
+var secondStep = document.querySelector('.js-activate');
+var submitBtn = document.querySelector('.js-submit');
+var thirdPage = document.querySelector('.event');
+var lastPage = document.querySelector('.footer');
+
+firstStep.addEventListener('click', function() {
+    firstPage.classList.add('passed');
+});
+
+if (secondStep) {
+    secondStep.addEventListener('click', function() {
+        firstPage.classList.add('passed-2');
+        secondPage.classList.add('passed');
+        setTimeout(function() {
+            return secondPage.classList.add('invisible');
+        }, 1000);
+
+        setTimeout(function() {
+            return thirdPage.classList.add('visible');
+        }, 500);
+    });
+};
+
+
+if (submitBtn) {
+    submitBtn.addEventListener('click', function() {
+        event.preventDefault();
+        thirdPage.classList.add('passed');
+
+        setTimeout(function() {
+            return thirdPage.classList.add('invisible');
+        }, 500);
+
+        setTimeout(function() {
+            return lastPage.classList.add('visible');
+        }, 500);
+
+        setTimeout(function() {
+            return lastPage.classList.add('active');
+        }, 1000);
+    });
+};
